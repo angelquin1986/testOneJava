8947baa162d4fa4d55bba69c175c64ff71df515a
angel <angel.quingaluisa@gmail.com>
2022-01-18 12:05:43 -0500

    source  invoke and order boats

--- demo/src/main/java/com/ec/qsof/demo/Boat.java
+++ demo/src/main/java/com/ec/qsof/demo/Boat.java
@@ -1,2 +1,27 @@
-package com.ec.qsof.demo;public class Boat {
+package com.ec.qsof.demo;
+
+public class Boat {
+   public Boat(String ship_id, Integer missionsSize) {
+      this.ship_id = ship_id;
+      this.missionsSize = missionsSize;
+   }
+
+   private String ship_id;
+   private Integer missionsSize;
+
+   public String getShip_id() {
+      return ship_id;
+   }
+
+   public void setShip_id(String ship_id) {
+      this.ship_id = ship_id;
+   }
+
+   public Integer getMissionsSize() {
+      return missionsSize;
+   }
+
+   public void setMissionsSize(Integer missionsSize) {
+      this.missionsSize = missionsSize;
+   }
 }
--- demo/src/main/java/com/ec/qsof/demo/Controller.java
+++ demo/src/main/java/com/ec/qsof/demo/Controller.java
@@ -1,2 +1,39 @@
-package com.ec.qsof.demo;public class Controller {
+package com.ec.qsof.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/example")
+class Controller {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/get")
+    public List<Boat> getHelloWord() {
+        ResponseEntity<Object[]> responseEntity =
+                restTemplate.getForEntity("https://api.spacexdata.com/v3/ships", Object[].class);
+        Object[] objects = responseEntity.getBody();
+        List<Boat> boatList = this.objToMap(objects).stream()
+                .map(boatMap ->new Boat(boatMap.get("ship_id").toString(),((List)boatMap.get("missions")).size()))
+                .collect(Collectors.toList());
+        boatList.sort(Comparator.comparing(Boat::getMissionsSize));
+        return boatList;
+    }
+
+    public List<Map> objToMap(Object[] objects) {
+        ObjectMapper mapper = new ObjectMapper();
+        return Arrays.stream(objects)
+                .map(object -> mapper.convertValue(object, Map.class))
+                .collect(Collectors.toList());
+    }
+
 }
